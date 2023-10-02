28dd9b85d21bbafbdc4bd4e55f61eb870ad718bc
Juan Valentin <valentin.juan05@gmail.com>
2023-10-02 20:42:01 -0300

    identacao em pedidoController

--- src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
+++ src/main/java/br/com/alurafood/pedidos/controller/PedidoController.java
@@ -18,49 +18,49 @@ import java.util.List;
 @RequestMapping("/pedidos")
 public class PedidoController {
 
-        @Autowired
-        private PedidoService service;
+    @Autowired
+    private PedidoService service;
 
-        @GetMapping()
-        public List<PedidoDto> listarTodos() {
-            return service.obterTodos();
-        }
+    @GetMapping()
+    public List<PedidoDto> listarTodos() {
+        return service.obterTodos();
+    }
 
-        @GetMapping("/{id}")
-        public ResponseEntity<PedidoDto> listarPorId(@PathVariable @NotNull Long id) {
-            PedidoDto dto = service.obterPorId(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<PedidoDto> listarPorId(@PathVariable @NotNull Long id) {
+        PedidoDto dto = service.obterPorId(id);
 
-            return  ResponseEntity.ok(dto);
-        }
+        return ResponseEntity.ok(dto);
+    }
 
     @GetMapping("/porta")
-    public String retornaPorta(@Value("${local.server.port}") String porta){
+    public String retornaPorta(@Value("${local.server.port}") String porta) {
         return String.format("Requisição respondida pela instância executando na porta %s", porta);
     }
 
-        @PostMapping()
-        public ResponseEntity<PedidoDto> realizaPedido(@RequestBody @Valid PedidoDto dto, UriComponentsBuilder uriBuilder) {
-            PedidoDto pedidoRealizado = service.criarPedido(dto);
+    @PostMapping()
+    public ResponseEntity<PedidoDto> realizaPedido(@RequestBody @Valid PedidoDto dto, UriComponentsBuilder uriBuilder) {
+        PedidoDto pedidoRealizado = service.criarPedido(dto);
 
-            URI endereco = uriBuilder.path("/pedidos/{id}").buildAndExpand(pedidoRealizado.getId()).toUri();
+        URI endereco = uriBuilder.path("/pedidos/{id}").buildAndExpand(pedidoRealizado.getId()).toUri();
 
-            return ResponseEntity.created(endereco).body(pedidoRealizado);
+        return ResponseEntity.created(endereco).body(pedidoRealizado);
 
-        }
+    }
 
-        @PutMapping("/{id}/status")
-        public ResponseEntity<PedidoDto> atualizaStatus(@PathVariable Long id, @RequestBody StatusDto status){
-           PedidoDto dto = service.atualizaStatus(id, status);
+    @PutMapping("/{id}/status")
+    public ResponseEntity<PedidoDto> atualizaStatus(@PathVariable Long id, @RequestBody StatusDto status) {
+        PedidoDto dto = service.atualizaStatus(id, status);
 
-            return ResponseEntity.ok(dto);
-        }
+        return ResponseEntity.ok(dto);
+    }
 
 
-        @PutMapping("/{id}/pago")
-        public ResponseEntity<Void> aprovaPagamento(@PathVariable @NotNull Long id) {
-            service.aprovaPagamentoPedido(id);
+    @PutMapping("/{id}/pago")
+    public ResponseEntity<Void> aprovaPagamento(@PathVariable @NotNull Long id) {
+        service.aprovaPagamentoPedido(id);
 
-            return ResponseEntity.ok().build();
+        return ResponseEntity.ok().build();
 
-        }
+    }
 }
